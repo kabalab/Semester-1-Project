2a269b1d6fb55d6ab4681be0b26a80e6fb2f7f10
kabalab <kaleb@theflings.com>
2025-12-03 09:40:59 -0800

    Updated lobby

--- Lobby.java
+++ Lobby.java
@@ -1,12 +1,40 @@
 import javax.swing.*;
+import java.awt.*;
 public class Lobby extends JPanel{
-    public Lobby() {
+    private final int width;
+    private final int height;
+    private final int carpetWidth;
+    private final Color red = new Color(122, 0, 0);
 
+    public Lobby(int width,int height) {
+        this.width = width;
+        this.height = height;
+        carpetWidth = width/15;
+    }
+
+    @Override
+    protected void paintComponent(Graphics p) {
+        super.paintComponent(p);
+        p.setColor(Color.BLACK);
+        p.fillRect(width/2-carpetWidth/2,0,carpetWidth,height/2);
+        p.fillRect(carpetWidth/2,height/2,width-(carpetWidth),carpetWidth);
+        p.fillArc(0,height/2,carpetWidth,carpetWidth,90,90);
+        p.fillArc(width-carpetWidth,height/2,carpetWidth,carpetWidth,0,90);
+        p.fillRect(0,height/2+carpetWidth/2,carpetWidth,height/2-carpetWidth/2);
+        p.fillRect(width-carpetWidth,height/2+carpetWidth/2,carpetWidth,height/2-carpetWidth/2);
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        Lobby lobby = new Lobby();
+        //idky but it goes over if the same width 
+        Lobby lobby = new Lobby(1200-16,600);
+        frame.setSize(1200,600);
+        frame.setTitle("Casino Lobby");
+        frame.setVisible(true);           
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);   
+        frame.setLocation(0,0);
+        frame.setResizable(true);
         frame.add(lobby);
         System.out.println("hello");
     }
